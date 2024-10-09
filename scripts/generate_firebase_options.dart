fcbb3f38ddd3bd0578a59c989e80064eb7494cbb
Radosław Lichtarski <lichtarski.radek@gmail.com>
2024-10-09 13:07:21 +0200

    fix firebase options files

--- scripts/generate_firebase_options.dart
+++ scripts/generate_firebase_options.dart
@@ -38,9 +38,45 @@ void main() {
       devIosClientId != null &&
       devIosBundleId != null) {
     final devContent = '''
-    import 'package:firebase_core/firebase_core.dart';
+    import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+    import 'package:flutter/foundation.dart'
+        show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
     class DefaultFirebaseOptions {
+      static FirebaseOptions get currentPlatform {
+        if (kIsWeb) {
+          throw UnsupportedError(
+            'DefaultFirebaseOptions have not been configured for web - '
+            'you can reconfigure this by running the FlutterFire CLI again.',
+          );
+        }
+        switch (defaultTargetPlatform) {
+          case TargetPlatform.android:
+            return android;
+          case TargetPlatform.iOS:
+            return ios;
+          case TargetPlatform.macOS:
+            throw UnsupportedError(
+              'DefaultFirebaseOptions have not been configured for macos - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+            );
+          case TargetPlatform.windows:
+            throw UnsupportedError(
+              'DefaultFirebaseOptions have not been configured for windows - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+            );
+          case TargetPlatform.linux:
+            throw UnsupportedError(
+              'DefaultFirebaseOptions have not been configured for linux - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+            );
+          default:
+            throw UnsupportedError(
+              'DefaultFirebaseOptions are not supported for this platform.',
+            );
+        }
+      }
+
       static const FirebaseOptions android = FirebaseOptions(
         apiKey: '$devAndroidApiKey',
         appId: '$devAndroidAppId',
