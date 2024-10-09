fec0c2d05cdf8d0ee08519d88ed9b59303b44eb7
Radosław Lichtarski <lichtarski.radek@gmail.com>
2024-10-09 20:15:39 +0200

    fix generating firebase options script

--- scripts/generate_firebase_options.dart
+++ scripts/generate_firebase_options.dart
@@ -95,6 +95,7 @@ void main() {
         iosBundleId: '$devIosBundleId',
       );
     }
+    
     ''';
     File('lib/firebase_options_dev.dart').writeAsStringSync(devContent);
   } else {
@@ -112,9 +113,45 @@ void main() {
       prodIosClientId != null &&
       prodIosBundleId != null) {
     final prodContent = '''
-    import 'package:firebase_core/firebase_core.dart';
+    import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+    import 'package:flutter/foundation.dart'
+        show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
     class DefaultFirebaseOptions {
+      static FirebaseOptions get currentPlatform {
+        if (kIsWeb) {
+          throw UnsupportedError(
+            'DefaultFirebaseOptions have not been configured for web - '
+            'you can reconfigure this by running the FlutterFire CLI again.',
+          );
+        }
+        switch (defaultTargetPlatform) {
+          case TargetPlatform.android:
+            return android;
+          case TargetPlatform.iOS:
+            return ios;
+          case TargetPlatform.macOS:
+            throw UnsupportedError(
+              'DefaultFirebaseOptions have not been configured for macos - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+            );
+          case TargetPlatform.windows:
+            throw UnsupportedError(
+              'DefaultFirebaseOptions have not been configured for windows - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+            );
+          case TargetPlatform.linux:
+            throw UnsupportedError(
+              'DefaultFirebaseOptions have not been configured for linux - '
+              'you can reconfigure this by running the FlutterFire CLI again.',
+            );
+          default:
+            throw UnsupportedError(
+              'DefaultFirebaseOptions are not supported for this platform.',
+            );
+        }
+      }
+
       static const FirebaseOptions android = FirebaseOptions(
         apiKey: '$prodAndroidApiKey',
         appId: '$prodAndroidAppId',
@@ -133,6 +170,7 @@ void main() {
         iosBundleId: '$prodIosBundleId',
       );
     }
+
     ''';
     File('lib/firebase_options_prod.dart').writeAsStringSync(prodContent);
   } else {
